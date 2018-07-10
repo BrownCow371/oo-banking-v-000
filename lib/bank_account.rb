afc1074a870d0d6f716a51db8224631b45c93672
Jamie Brown <jmbrowncow@gmail.com>
2018-07-10 16:39:59 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,14 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :account_name
+  
+  def initialize(account_name)
+    @account_name=account_name
+    self.balance = 1000
+    self.status= "open"
+  end
 
+  def deposit(amount)
+    
+  end
 end
